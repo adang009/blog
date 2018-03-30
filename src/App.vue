0c5ddd528958db1dbc6e056ee4424999b4591d95
党文良 <cooluid@dangwenliangdeMacBook-Pro.local>
2018-03-31 00:07:00 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" width="100" height="100">
+    <tab></tab>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import Tab from './components/tab/tab'
+
+  export default {
+    name: 'App',
+    components: {
+      Tab
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/tab/tab.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="tab">
+    <router-link tag="div" class="tab-item" to="/">
+      <span class="tab-link">首页</span>
+    </router-link>
+    <souter-link tag="div" class="tab-item" to="/">
+      <span class="tab-link">关于</span>
+    </souter-link>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .tab
+    display: flex
+    height: 44px
+    width: 100px
+    line-height: 44px
+    font-size: 16px
+    .tab-item
+      flex: 1
+      text-align: center
+      .tab-link
+        padding-bottom: 5px
+        color: #cccc
+      &.router-link-active
+        .tab-link
+          color: #8468cc
+          border-bottom: 2px
+</style>
